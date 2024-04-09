960f584e94b2dcf128dc3caded034ff1595344ee
Shyam Sundar <shyamsundarv00073@gmail.com>
2024-04-09 12:55:49 +0530

    First Commit

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,14 @@
+function NotFound () {
+    return (
+        <div>
+            <h1>
+                404 Not Found
+            </h1>
+            <p>
+                The page doesnot exist :(
+            </p>
+        </div>
+    )
+}
+
+export default NotFound
